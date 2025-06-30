3391f9e81517826be8fca6ca7a604dc702dcb8de
Glitch-op <karanyadavop0@gmail.com>
2025-06-30 13:36:57 +0530

    firebase changed

--- src/Service/firebaseConfig.jsx
+++ src/Service/firebaseConfig.jsx
@@ -9,7 +9,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD99ZuQKZWKHC77rSnMwZkt596-wPxTOkA",
+  apiKey: "FIREBASE_API_KEY",
   authDomain: "ai-trip-planner-9ccbc.firebaseapp.com",
   projectId: "ai-trip-planner-9ccbc",
   storageBucket: "ai-trip-planner-9ccbc.firebasestorage.app",
